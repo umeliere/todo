53f90b7a2cdf02427838838d25be881d472d761f
umeliere <umeliere@yandex.ru>
2023-04-01 17:46:30 +0300

    add favicon.ico

--- todo/urls.py
+++ todo/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path("users/", include("users.urls")),
     path("users/", include("django.contrib.auth.urls")),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
 ]
 
 if settings.DEBUG:
