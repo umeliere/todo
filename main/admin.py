b68bd81e6e944f076f675072073bdfd6373371dc
umeliere <umeliere@yandex.ru>
2023-04-09 19:34:19 +0300

    admin.py was rewritten

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,33 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Tasks)
-admin.site.register(Category)
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+
+        if not change:
+            obj.user = request.user
+
+        super(TasksAdmin, self).save_model(
+            request=request,
+            obj=obj,
+            form=form,
+            change=change
+        )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+
+        if not change:
+            obj.user = request.user
+
+        super(CategoryAdmin, self).save_model(
+            request=request,
+            obj=obj,
+            form=form,
+            change=change
+        )
