71e5c84dc7261535cf5b4dac21004798aa8b627d
umeliere <umeliere@yandex.ru>
2023-04-11 16:58:43 +0300

    some changes...

--- api/routers.py
+++ api/routers.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import CategoryView, TasksView
 
+
 router = DefaultRouter()
 router.register(r"tasks", TasksView, basename='tasks')
 router.register(r"category", CategoryView, basename='category')
